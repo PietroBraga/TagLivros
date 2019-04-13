b3fa01df702f5d3c7f0047d7bf2e2170c95e36d4
pietrobraga@hotmail.com <aa685492>
2019-04-13 10:30:58 -0300

    Adding WebClient

--- src/test/java/com/taglivros/core/imagens/TestesDeImagens.java
+++ src/test/java/com/taglivros/core/imagens/TestesDeImagens.java
@@ -1,18 +1,46 @@
 package com.taglivros.core.imagens;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebClientOptions;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptJobManager;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestesDeImagens {
     @Test
     public void homePage() throws Exception {
 
-            final WebClient webClient = new WebClient();
-            final HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
+            final WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        WebClientOptions option = webClient.getOptions();
+            webClient.waitForBackgroundJavaScript(20000);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            option.setJavaScriptEnabled(true);
+            option.setDownloadImages(true);
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.setCssErrorHandler(new SilentCssErrorHandler());
+            HtmlPage page = null;
+            try{
+                page = webClient.getPage("http://elemaisvelhoelamaisnova.blogspot.com/");
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+            boolean test = false;
+            List<HtmlImage> elements = page.getByXPath("//img");
+            for (HtmlImage element: elements
+                 ) {
+                test = element.isComplete();
+            }
             Assert.assertEquals("HtmlUnit - Welcome to HtmlUnit", page.getTitleText());
 
             final String pageAsXml = page.asXml();
