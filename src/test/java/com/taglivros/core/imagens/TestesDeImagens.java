cc18ca7659510eda6e92214c28bb948d83f33139
pietrobraga@hotmail.com <aa685492>
2019-04-22 08:11:42 -0300

    Adding Image Load Validation

--- src/test/java/com/taglivros/core/imagens/TestesDeImagens.java
+++ src/test/java/com/taglivros/core/imagens/TestesDeImagens.java
@@ -1,9 +1,6 @@
 package com.taglivros.core.imagens;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebClientOptions;
+import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -15,10 +12,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Time;
+import java.util.*;
 
 public class TestesDeImagens {
     @Test
@@ -58,4 +53,68 @@ public class TestesDeImagens {
         }
         Assert.assertTrue(!results.values().toString().contains("f"));
     }
-}
+
+        @Test
+        public void mobile() throws Exception {
+            BrowserVersion bv = new BrowserVersion.BrowserVersionBuilder(BrowserVersion.CHROME)
+                    .setApplicationCodeName("Mozilla")
+                    .setApplicationMinorVersion("0")
+                    .setApplicationVersion("5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Mobile Safari/537.36")
+                    .setVendor("")
+                    .setBrowserLanguage("pt-BR")
+                    .setCpuClass("x86")
+                    .setOnLine(true)
+                    //.setPlatform("Linux; Android 7.0; SM-G930V Build/NRD90M")
+                    .setSystemLanguage("pt-BR")
+                    .setSystemTimezone(TimeZone.getTimeZone("America/New_York"))
+                    .setUserLanguage("pt-BR")
+                    .setHtmlAcceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                    .setImgAcceptHeader("image/webp,image/apng,image/*,*/*;q=0.8")
+                    .setCssAcceptHeader("\"text/css,*/*;q=0.1\"")
+                    .setScriptAcceptHeader("*/*")
+                    .setXmlHttpRequestAcceptHeader("*/*")
+                    .setUserAgent("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Mobile Safari/537.36")
+                    .build();
+
+            final WebClient webClient = new WebClient(bv);
+            WebClientOptions option = webClient.getOptions();
+            webClient.getOptions().setScreenHeight(736);
+            webClient.getOptions().setScreenWidth(414);
+            webClient.waitForBackgroundJavaScript(20000);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            option.setJavaScriptEnabled(true);
+            option.setDownloadImages(true);
+
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.setCssErrorHandler(new SilentCssErrorHandler());
+            HtmlPage page = null;
+            HashMap<String, Boolean> results = new HashMap<String, Boolean>();
+            try{
+                page = webClient.getPage("http://taglivros.com.br");
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+
+            List<HtmlImage> images = page.getByXPath("//img");
+            for (HtmlImage image: images) {
+                String src = image.getAttribute("src");
+                if (src.equals("")){
+                    src = image.getAttribute("data-src");
+                }
+                if (src.equals("")){
+                    src = image.getAttribute("data-lazy-load");
+                }
+                try {
+                    image.getImageReader();
+                }
+                catch (Exception e) {}
+                results.put(src, image.isComplete());
+            }
+
+            for (Map.Entry<String, Boolean> result: results.entrySet()) {
+                System.out.println(result.getKey() + " : " + result.getValue());
+            }
+            Assert.assertTrue(!results.values().toString().contains("f"));
+        }
+    }
